bfbee031be41916d57d99b26161be5d6c23760b5
DrAtomic <leo.ward.garcia@gmail.com>
2020-09-09 09:12:26 -0700

    first commit

---
+++ commander.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from itertools import permutations
+import sys
+
+def search(values,searchFor):
+    for k in values.get('Colors'):
+        if searchFor in k:
+            return values
+
+
+def color_or(dictionary,color):
+    temp =[]
+    for color in colors:
+        for i in dictionary:
+            temp.append(search(i,color))
+    result = [x for x in temp if x is not None]
+    return result
+
+
+
+def fetch(list_dictionaries):
+    if any(t== "fetch" for t in list_dictionaries.values()):
+        return list_dictionaries
+
+
+def color_and(dictionary, color):
+    tesat = ["".join(map(str,comb)) for comb in permutations(color,2)]
+    for c in tesat:
+        for i in dictionary:
+            if c in i.get('Colors'):
+                temp.append(i)
+    result = [x for x in temp if x is not None]
+    return(result)
+
+if __name__ == "__main__":
+    
+    df = pd.read_csv('cards.csv')
+    test = df.to_dict(orient='records')
+    name = str(sys.argv[1])
+    colors = list(sys.argv[2])
+    temp =[]
+    x = color_or(test, colors)
+    fetchlands=[]
+    for i in x:
+        fetchlands.append(fetch(i))
+
+    fetchlands = [t for t in fetchlands if t is not None]
+    fetchlands = [dict(t) for t in {tuple(d.items()) for d in fetchlands}]
+    y = color_and(test,colors)
+
+    listoflands = []
+
+    for i in fetchlands:
+        listoflands.append(i.get('Title'))
+
+    for i in y:
+        listoflands.append(i.get('Title'))
+
+
+    mylist = list(dict.fromkeys(listoflands))
+    mylist.extend(['Mana Crypt','Command Tower','Ancient Tomb','Sol Ring','Arcane Signet'])
+    f = open(str(name)+".cod",'w')
+    f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<cockatrice_deck version=\"1\">\n\t<deckname></deckname>\n\t<comments></comments>\n\t<zone name=\"main\">\n")
+    for name in mylist:
+        f.write("\t\t<card number=\"1\" name=\""+str(name)+"\"/>\n")
+    #this is where all the normal stuff goes
+    f.write("\t</zone>\n</cockatrice_deck>")
+    f.close()
+
